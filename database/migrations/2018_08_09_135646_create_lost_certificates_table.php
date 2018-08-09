d258e41fa9dc23a5089b757f5bf670572d459faf
RobiBerhe <berherobi@gmail.com>
2018-08-09 06:57:57 -0700

    -adding lost -certificate migration and models

---
+++ database/migrations/2018_08_09_135646_create_lost_certificates_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLostCertificatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lost_certificates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lost_certificates');
+    }
+}
