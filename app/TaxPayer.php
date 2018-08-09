51da62011e1e1aeee3a692e3feff333578881b68
RobiBerhe <berherobi@gmail.com>
2018-08-09 06:51:38 -0700

    -model relnships

--- app/TaxPayer.php
+++ app/TaxPayer.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class TaxPayer extends Model
 {
     //
+
+    public function addresses(){
+        return $this->hasMany('App\TaxPayerAddress');
+    }
+
+    public function bussinessLegalStatus(){
+        return $this->hasOne('App\BusinessLegalStatus');
+    }
+
+    public function commitment(){
+        return $this->hasOne('App\Commitment');
+    }
+
+
 }
